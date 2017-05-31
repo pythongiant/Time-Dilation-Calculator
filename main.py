ea6e952284d72d1b52078628cb74cb5f4fe17dd4
pythongiant <srihari.unnikrishnan@gmail.com>
2017-05-31 17:26:36 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import math
+print("\t Welcome To the Time Dilation Calculator")
+t=input("Elapsed Time at body(seconds): ")
+v=input("Speed(km/s): ")
+speed_of_light=299792.458
+print(int(t)/math.sqrt(1-(int(v)**2/speed_of_light**2)))
